9dd343d55c4c07553504116e7b303060e2e705ad
adriaVH <adriavargashernandez@gmail.com>
2025-03-11 12:30:52 +0100

    finished ex1

---
+++ src/ex1/main/Main.java
@@ -0,0 +1,60 @@
+package ex1.main;
+
+import ex1.classes.Month;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class Main {
+    public static void main(String[] args) {
+      /* Crea una classe anomenada Month amb un atribut "name" (que emmagatzemarà el nom del mes de l'any).
+      Afegeix 11 objectes Month (cadascun amb el seu atribut diferent) en un ArrayList, a excepció de l'objecte
+      amb atribut "Agost". Després, efectua la inserció en el lloc que correspon a aquest mes i demostra que
+      l’ArrayList manté l'ordre correcte.
+   Converteix l’ArrayList de l’exercici anterior en un HashSet i assegura’t que no permet duplicats
+   Recorre la llista amb un for i amb un iterador.*/
+
+        ArrayList<Month> months = new ArrayList<>();
+        String[] monthNames = {
+                "January", "February", "March", "April", "May", "June", "July", "September", "October", "November", "December"
+        };
+        for (int i = 0; i < 11; i++) {
+            months.add(new Month(monthNames[i]));
+        }
+        System.out.println("Before insertion: ");
+
+        for (Month month : months) {
+            System.out.print(month.getName() + " ");
+        }
+        months.add(7, new Month("August"));
+
+        System.out.println("\nAfter \"August\" insertion: ");
+        for (Month month : months) {
+            System.out.print(month.getName() + " ");
+        }
+
+        months.add(new Month("January"));
+        HashSet<Month> hashSetMonths = new HashSet<>(months);
+
+        System.out.println("\nHashSet Months after a duplicate \"January\" by attribute: ");
+        for (Month month : hashSetMonths) {
+            System.out.print(month.getName() + " ");
+        }
+        Iterator<Month> it = months.iterator();
+        Iterator<Month> itHash = hashSetMonths.iterator();
+
+        System.out.println("\nIteration ArrayList: ");
+        while (it.hasNext()){
+            System.out.print(it.next().getName()+" ");
+        }
+
+        System.out.println("\nIteration HashSet: ");
+        while (itHash.hasNext()){
+            System.out.print(itHash.next().getName()+" ");
+        }
+        //Recorrer amb un FOR ja l'he recorregut abans, per això no le posat
+
+    }
+
+}
--- src/ex2/main/Main.java
+++ src/ex2/main/Main.java
@@ -1,4 +1,4 @@
-package main;
+package ex2.main;
 
 public class Main {
     public static void main(String[] args) {
