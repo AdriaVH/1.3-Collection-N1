7c6cae06f7b594f57c49119331b123a926e7cfd0
AdriaVH <adriavargashernandez@gmail.com>
2025-03-12 21:32:30 +0100

    ex3 finished

--- src/ex3/main/Main.java
+++ src/ex3/main/Main.java
@@ -1,60 +1,77 @@
 package ex3.main;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
-       /* Donat el fitxer countrties.txt (revisa l'apartat recursos) que conté països i capitals.
-       El programa ha de llegir el fitxer i guardar les dades en un HashMap<String, String>. El programa
-       demana el nom de l’usuari/ària, i després ha de mostrar un país de forma aleatòria, guardat en el
-       HashMap. Es tracta que l’usuari/ària ha d’escriure el nom de la capital del país en qüestió. Si
-       l’encerta se li suma un punt. Aquesta acció es repeteix 10 vegades. Un cop demanades les capitals
-       de 10 països de forma aleatòria, llavors s’ha de guardar en un fitxer anomenat classificacio.txt,
-       el nom de l’usuari/ària i la seva puntuació. */
+    static Scanner sc = new Scanner(System.in);
 
-        String filePath = "C:\\Users\\formacio\\Desktop\\1.3-Java-Collection-N1\\rsc\\countries.txt";
-
+    public static void main(String[] args) {
+        String filePath = "src/rsc/countries.txt";
         File file = new File(filePath);
         HashMap<String, String> countriesHashMap = new HashMap<>();
-        Scanner sc = new Scanner(System.in);
+        int numberOfQuestions = 10; // change to 3 for testing
 
-        Scanner fileSc = new Scanner(file);
-        while (fileSc.hasNext()) {
-            String line = fileSc.nextLine();
-            String[] splittedLine = line.split("\\s");
-            countriesHashMap.put(splittedLine[0], splittedLine[1]);
-        }
+        fileToHashMap(file, countriesHashMap);
         System.out.println("Introduce your user name: ");
-        String user = sc.next();
-        List<String> keys = new ArrayList<>(countriesHashMap.keySet());
-        List<Boolean> answers = new ArrayList<>();
-        int points = 0;
-       for (int i = 0; i < 3; i++){
-           String randomCountry = keys.get(new Random().nextInt(keys.size()));
-           System.out.println("What's the capital of "+randomCountry);
-           String userCapital = sc.next();
-           compareWithMap(randomCountry,userCapital,answers, countriesHashMap);
-           points = getPoints(answers);
-
-       }
-
-        System.out.println(points);
-
+        String user = sc.nextLine();
+        int points = guessTheCapitals(countriesHashMap, numberOfQuestions);
+        saveToFile(user, points, numberOfQuestions);
     }
-    public static void compareWithMap (String randomCountry,String userCapital, List<Boolean> answers,
-                                       HashMap<String, String> countriesHashMap) {
-    String value = countriesHashMap.get(randomCountry);
-    Boolean answer = value.equalsIgnoreCase(userCapital);
-        answers.add(answer);
-    }
-    public static int getPoints(List<Boolean> answers) {
-        int points = 0;
-        for (Boolean value : answers){
-            if (value){
-                points += points + 1;
+
+    public static void fileToHashMap(File file, HashMap<String, String> countriesHashMap) {
+        try {
+            Scanner fileSc = new Scanner(file);
+            while (fileSc.hasNext()) {
+                String line = fileSc.nextLine();
+                String[] splittedLine = line.split("\\s");
+                splittedLine[0] = splittedLine[0].replace("_", " ");
+                splittedLine[1] = splittedLine[1].replace("_", " ");
+
+                countriesHashMap.put(splittedLine[0], splittedLine[1]);
             }
-        }return (points);
+            fileSc.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + file.getPath()
+                    + "\nCheck that the path is correct and the file exists");
+            System.exit(1);
+        }
+    }
+
+
+    public static int guessTheCapitals(HashMap<String, String> countriesHashMap, int times) {
+        int result = 0;
+        List<String> keys = new ArrayList<>(countriesHashMap.keySet());
+        String randomCountry;
+        String userCapital;
+        Collections.shuffle(keys);
+
+        for (int i = 0; i < times; i++) {
+            randomCountry = keys.get(i);
+            System.out.println("What's the capital of " + randomCountry + "?");
+            userCapital = sc.nextLine();
+            if(compareWithMap(randomCountry, userCapital, countriesHashMap)){
+                result++;
+            }
+        }
+        return result;
+    }
+
+    public static boolean compareWithMap(String randomCountry, String userCapital,
+                                      HashMap<String, String> countriesHashMap) {
+        String value = countriesHashMap.get(randomCountry);
+        return value.equalsIgnoreCase(userCapital);
+    }
+
+    public static void saveToFile(String user, int points, int times) {
+        String fileName = "classificacio.txt";
+        try {
+            FileWriter writer = new FileWriter(fileName, true);
+            writer.write(user + " -> " + points + "/" + times + " points\n");
+            writer.close();
+            System.out.println("Score successfully saved to \"" + fileName + "\"");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
